25d406aae31dba4e6da3b445e85e77c01ffb6728
ShingoOhsugi <ref.3570.40@gmail.com>
2014-07-15 23:11:57 +0900

    不足モジュール追加

---
+++ houkokuChecker/houkokuChecker/page/WelcomePage.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace houkokuChecker
+{
+    /// <summary>
+    /// WelcomePage.xaml の相互作用ロジック
+    /// </summary>
+    public partial class WelcomePage : Page
+    {
+        public WelcomePage()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// 開始ボタン押下
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnStart_Click(object sender, RoutedEventArgs e)
+        {
+            this.NavigationService.Content = new ConfigPage();
+        }
+
+    }
+}
